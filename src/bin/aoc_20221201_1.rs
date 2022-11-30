cbfcccf489faf638b7ea04269ddbfd6672d3685e
Benno Richters <b.richters@gmail.com>
2022-11-30 18:34:48 +0100

    set up to start

---
+++ src/bin/aoc_20221201_1.rs
@@ -0,0 +1,19 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|e| e.expect("Could not parse line"))
+        .collect()
+}
+
+fn main() {
+    let lines = lines_from_file("in");
+
+    println!("{}", lines[0]);
+}

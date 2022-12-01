20ebc8017df48d329082cd75f13c47d097303e44
Benno Richters <b.richters@gmail.com>
2022-12-01 06:24:19 +0100

    nicer

--- src/bin/aoc_20221201_2.rs
+++ src/bin/aoc_20221201_2.rs
@@ -15,20 +15,12 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 fn main() {
     let lines = lines_from_file("in");
 
-    let mut iter = lines.split(|e| e.is_empty());
+    let iter = lines.split(|e| e.is_empty());
 
     let mut all: Vec<i32> = Vec::new();
-    loop {
-        let elf = iter.next();
-        if elf.is_none() {
-            break;
-        }
-
-        let mut total = 0;
-        for c in elf.unwrap() {
-            total += c.parse::<i32>().unwrap();
-        }
-        all.push(total);
+    for elf in iter {
+        let content: Vec<i32> = elf.iter().map(|e| e.parse::<i32>().unwrap()).collect();
+        all.push(content.iter().sum());
     }
 
     all.sort();

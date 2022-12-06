48f4f6f7c99c0019062407915ff2d91885c4446b
Benno Richters <b.richters@gmail.com>
2022-12-06 07:00:41 +0100

    day 6

---
+++ src/bin/day06p2.rs
@@ -0,0 +1,29 @@
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|e| e.expect("could not parse line"))
+        .collect()
+}
+
+fn main() {
+    let lines = lines_from_file("in");
+
+    let line = &lines[0];
+    let marker = 14;
+    for i in 0..(line.len() - marker) {
+        let distinct: HashSet<char> = line[i..(i + marker)].chars().collect();
+        if distinct.len() == marker {
+            println!("{}", i + marker);
+            break;
+        }
+    }
+}
+

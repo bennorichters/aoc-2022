7ad0718834872a8ed288e91a8bbdfa4518e63bdc
Benno Richters <b.richters@gmail.com>
2022-12-14 12:28:17 +0100

    day 13 part 1

--- src/bin/day13p1.rs
+++ src/bin/day13p1.rs
@@ -1,6 +1,3 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
@@ -18,37 +15,31 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 }
 
 fn main() {
-    // solve();
-
-    let a = json::parse("[[],7]").unwrap();
-    let b = json::parse("[[3]]").unwrap();
-
-    let result = check(&a, &b);
-    println!("{}", result);
+    solve();
 }
 
-// fn solve() {
-//     let lines = lines_from_file("tin");
-//     let puzzle_parts = lines.split(|e| e.is_empty());
+fn solve() {
+    let lines = lines_from_file("in");
+    let puzzle_parts = lines.split(|e| e.is_empty());
 
-//     let mut result = 0;
-//     for (i, batch) in puzzle_parts.enumerate() {
-//         let j1 = json::parse(&batch[0]).unwrap();
-//         let j2 = json::parse(&batch[1]).unwrap();
+    let mut result = 0;
+    for (i, batch) in puzzle_parts.enumerate() {
+        let j1 = json::parse(&batch[0]).unwrap();
+        let j2 = json::parse(&batch[1]).unwrap();
 
-//         if check(&j1, &j2) {
-//             result += i + 1;
-//         }
-//     }
+        if check(&j1, &j2).unwrap() {
+            result += i + 1;
+        }
+    }
 
-//     println!("{:?}", result);
-// }
+    println!("{:?}", result);
+}
 
-fn check(j1: &JsonValue, j2: &JsonValue) -> bool {
+fn check(j1: &JsonValue, j2: &JsonValue) -> Option<bool> {
     for (i, m1) in j1.members().enumerate() {
         let m2op = j2.members().nth(i);
         if m2op.is_none() {
-            return false;
+            return Some(false);
         }
         let m2 = m2op.unwrap();
 
@@ -57,26 +48,35 @@ fn check(j1: &JsonValue, j2: &JsonValue) -> bool {
             let n2 = m2.as_u8().unwrap();
 
             if n1 < n2 {
-                return true;
+                return Some(true);
             } else if n1 > n2 {
-                return false;
+                return Some(false);
             }
         } else if m1.is_number() {
             let mut sub_arr = JsonValue::new_array();
             sub_arr.push(m1.clone()).unwrap();
-            if !check(&sub_arr, m2) {
-                return false;
+            let sub = check(&sub_arr, m2);
+            if sub.is_some() {
+                return sub;
             }
         } else if m2.is_number() {
             let mut sub_arr = JsonValue::new_array();
             sub_arr.push(m2.clone()).unwrap();
-            if !check(m1, &sub_arr) {
-                return false;
+            let sub = check(m1, &sub_arr);
+            if sub.is_some() {
+                return sub;
             }
-        } else if !check(m1, m2) {
-            return false;
+        } else {
+            let sub = check(m1, m2);
+            if sub.is_some() {
+                return sub;
+            }
         }
     }
 
-    true
+    if j1.members().len() == j2.members().len() {
+        None
+    } else {
+        Some(true)
+    }
 }
